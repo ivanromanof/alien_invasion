d0642c52901215dda253f2f5d744155e355b89fc
ivanromanof <ivanromanov123@yandex.ru>
2020-05-12 16:21:06 +0300

    fix

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,13 +30,13 @@ class AlienInvasion:
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
-    def _check_keydown_events(self, event):
+    def _check_keydown_events(self, event): 
+        if event.key == pygame.K_F1:
+            sys.exit()
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-        elif event.key == pygame.K_q:
-            sys.exit()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
